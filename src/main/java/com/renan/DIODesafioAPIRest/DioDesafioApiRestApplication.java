f8accdcb23e7c5272b620cebe23097a3473bae78
RenanVtr <renanvitorg@hotmail.com>
2023-10-22 23:07:00 -0300

    first commit

---
+++ src/main/java/com/renan/DIODesafioAPIRest/DioDesafioApiRestApplication.java
@@ -0,0 +1,18 @@
+package com.renan.DIODesafioAPIRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DioDesafioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioApiRestApplication.class, args);
+
+
+
+
+	}
+
+}
